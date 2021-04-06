61fae10801a4706bc8d060c93c8ec1bec722324d
Yanis Madani <madaniyanis@gmail.com>
2021-04-06 18:01:51 +0200

    page d'acceuil

--- membres/index.php
+++ membres/index.php
@@ -2,5 +2,37 @@
 require_once 'entete.php';
   ?>
   <h1 class="text-center">Bienvenue sur Quizz Mania!</h1>
+
+  <h3 class="text-center">Les Nouveaux Quizz:</h3>
+        
+      <div class="container text-center">
+          <div class="card mb-3">
+            <div class="row g-0">
+              <div class="col-md-4 imgdiv">
+                <img src="images/quizz.png">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+     
+      <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown button
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+    </div>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
 </body>
 </html>
